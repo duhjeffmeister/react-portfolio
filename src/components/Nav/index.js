8e911681494603a1414f478ead0a2206dc9541be
Jeff Small <jeffmeister1990@gmail.com>
2022-10-08 23:51:22 -0400

    Created index.js in Nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+function Nav(props) {
+  const { setContactSelected, setPortfolioSelected, setResumeSelected } = props
+
+  // conditional rendering
+  return (
+    <header className="header-container">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera">
+            {' '}
+          </span>{' '}
+          Jeff Small
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a
+              className="nav-item"
+              data-testid="about"
+              href="#about"
+              onClick={() => {
+                setContactSelected(false)
+                setPortfolioSelected(false)
+                setResumeSelected(false)
+              }}
+            >
+              About me
+            </a>
+            <a
+              href="#portfolio"
+              className="nav-item"
+              data-testid="portfolio"
+              onClick={() => {
+                setContactSelected(false)
+                setPortfolioSelected(true)
+                setResumeSelected(false)
+              }}
+            >
+              Portfolio
+            </a>
+            <a
+              href="#contactMe"
+              className="nav-item"
+              data-testid="contactMe"
+              onClick={() => {
+                setContactSelected(true)
+                setPortfolioSelected(false)
+                setResumeSelected(false)
+              }}
+            >
+              Contact Me
+            </a>
+            <a
+              href="#resume"
+              className="nav-item"
+              data-testid="resume"
+              onClick={() => {
+                setContactSelected(false)
+                setPortfolioSelected(false)
+                setResumeSelected(true)
+              }}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
