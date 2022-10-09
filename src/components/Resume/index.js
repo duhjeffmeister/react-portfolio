e1cfeea5792d23a5e047b261832a5a3b5e9d3b21
Jeff Small <jeffmeister1990@gmail.com>
2022-10-08 23:51:44 -0400

    Created index.js in Resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Resume() {
+  return (
+    <div className="my-2">
+      <p>
+        RESUME{' '}
+        <a href="https://www.linkedin.com/in/jlsmall1990/">
+          {' '}
+          Download
+        </a>
+      </p>
+      <p>Technologies</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>MySQL</li>
+        <li>GraphQL</li>
+        <li>NoSQL</li>
+        <li>Sequelize</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Resume
